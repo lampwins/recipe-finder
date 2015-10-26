9270f8a97648500797d8ff3ba6daeeedf77018de
John Anderson <lampwins@gmail.com>
2015-10-26 21:30:17 +0000

    migrated schema

--- db/migrate/20151025033401_create_recipes.rb
+++ db/migrate/20151025033401_create_recipes.rb
@@ -2,14 +2,14 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :title
-      t.int :cook_time
+      t.integer :cook_time
       t.string :directions
       t.timestamps null: false
     end
     create_table :ingredients do |t|
       t.string :common_name
       t.string :name
-      t.int :recipe
+      t.integer :recipe
     end
     add_foreign_key :ingredients, :recipes,  column: :recipe
   end
