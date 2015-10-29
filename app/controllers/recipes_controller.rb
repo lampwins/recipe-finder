16d59c39a5a8d5cd1956d7139f308a2f043ef393
John Anderson <lampwins@gmail.com>
2015-10-29 22:42:30 +0000

    add methods to model to return unique ingredients

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,18 @@ class RecipesController < ApplicationController
     recipe.title = params[:title]
     recipe.cook_time = params[:cook_time]
     recipe.directions = params[:directions]
-    recipe.main_ingredient = params[:protein]
+    recipe.main_ingredient = params[:main_ingredient]
     recipe.side_ingredient = params[:side_ingredient]
     recipe.save!
   
     flash[:notice] = "Successfuly added " + recipe.title
     redirect_to :root
+    
+  end
+  
+  def new
+    @main_ingredients = Recipe.allMainIngredients
+    @side_ingredients = Recipe.allSideIngredients
     
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ActiveRecord::Base
+    
+    def self.allMainIngredients
+        Recipe.uniq.pluck(:main_ingredient)
+    end
+    
+    def self.allSideIngredients
+        Recipe.uniq.pluck(:side_ingredient)
+    end
 end
