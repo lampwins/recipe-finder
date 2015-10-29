6e96ab9efa7c159f6b446bdac485324df6a7c45e
John Anderson <lampwins@gmail.com>
2015-10-29 04:20:58 +0000

    rework model and create add page full mvc

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,19 @@
 class RecipesController < ApplicationController
+  
   def index
+  end
+  
+  def create
+    recipe = Recipe.new()
+    recipe.title = params[:title]
+    recipe.cook_time = params[:cook_time]
+    recipe.directions = params[:directions]
+    recipe.main_ingredient = params[:protein]
+    recipe.side_ingredient = params[:side_ingredient]
+    recipe.save!
+  
+    flash[:notice] = "Successfuly added " + recipe.title
+    redirect_to :root
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :recipes
+    resources :recipes, only: %w(new show create)
   # Example resource route with options:
   #   resources :products do
   #     member do
