5aa09b465cb5804140644153c2f55ab90baa6f4b
abphil <andrewphillipsb@gmail.com>
2015-11-10 00:01:05 +0000

    Sort by cook time

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
   def index
     @main_ingredients = Recipe.allMainIngredients
     @side_ingredients = Recipe.allSideIngredients
+    @counter = 0
     
   end
   
@@ -27,27 +28,31 @@ class RecipesController < ApplicationController
     
   end
   
+
   def show
     if params[:main_ingredient] == "select" || params[:side_ingredient] == "select"
       flash[:notice] = "YOU MUST SPECIFY BOTH INGREDIENTS"
       redirect_to(:root)
       return
     end
+
     
     if params.has_key?(:ids)
       @ids = params[:ids].split(",")
       @recipe = Recipe.where("id NOT IN (?)", @ids)
+      
     else
       @ids = []
       @recipe = Recipe
     end
-    
+    @recipe = @recipe.order(cook_time: :asc)
     @recipe = @recipe.find_by main_ingredient: params[:main_ingredient], side_ingredient: params[:side_ingredient]
     if @recipe.nil? and !@ids.empty?
       flash[:notice] = "There are no more recipes for these ingredients"
       redirect_to(:root)
       return
     elsif @recipe.nil?
+
       flash[:notice] = "There is no recipe with these ingredients"
       redirect_to(:root)
       return
