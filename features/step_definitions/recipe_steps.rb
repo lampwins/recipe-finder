3a00edafc9c9d93380796de798d9d18fcb1214a2
mediamogulx <mediamogulx@hotmail.com>
2015-11-14 21:22:48 +0000

    Phil: Cuke Test Update for find-another

--- features/step_definitions/recipe_steps.rb
+++ features/step_definitions/recipe_steps.rb
@@ -3,3 +3,14 @@ Given /the following recipes exist/ do |recipe_table|
         Recipe.create!(recipe)
     end
 end
+
+When /I search for "(.*)" and "(.*)"/ do |i1, i2|
+    step "I select \"#{i1}\" from \"main_ingredient\""
+    step "I select \"#{i2}\" from \"side_ingredient\""
+    step "I press \"Submit\""
+end
+
+Then /I should see the "(.*)" recipe/ do |r1|
+    step "I should be on the recipe page"
+    step "I should see \"#{r1}\""
+end
