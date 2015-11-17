09359dd1fac4734ae5be109e3b7b6293bfd9181b
John Anderson <lampwins@gmail.com>
2015-11-17 05:14:39 +0000

    rspec tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029025831) do
+ActiveRecord::Schema.define(version: 20151117000251) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151029025831) do
     t.string   "side_ingredient"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "likes"
   end
 
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -9,4 +9,31 @@ RSpec.describe RecipesController, type: :controller do
     end
   end
 
+  describe "#show" do
+    before(:each) do
+      @recipe = Recipe.create! :main_ingredient => "Chicken", :side_ingredient => "Rice", :directions => "cook it", :cook_time => 30, :title => "Test", :id => 1
+      @recipe = Recipe.create! :main_ingredient => "Chicken", :side_ingredient => "Rice", :directions => "cook it", :cook_time => 15, :title => "Test2", :id => 2
+    end
+    it 'accepts params' do
+      get :show, :main_ingredient => 'Chicken', :side_ingredient => 'Rice'
+      expect(response).to have_http_status(:success)
+    end
+    it "works for clicking choose another" do
+      get :show, :main_ingredient => 'Chicken', :side_ingredient => 'Rice', :ids => "1"
+      expect(response).to have_http_status(:success)
+    end
+    it "sends back to home page if no more exist" do
+      get :show, :main_ingredient => 'Chicken', :side_ingredient => 'Rice', :ids => "1,2"
+      expect(controller.flash[:notice]).to eq("There are no more recipes for these ingredients")
+    end
+    it "sends back to home if nothing matches" do
+      get :show, :main_ingredient => 'Pork', :side_ingredient => 'Rice'
+      expect(controller.flash[:notice]).to eq("There is no recipe with these ingredients")
+    end
+    it "sends back if invalid input" do
+      get :show, :main_ingredient => 'select', :side_ingredient => 'select'
+      expect(controller.flash[:notice]).to eq("YOU MUST SPECIFY BOTH INGREDIENTS")
+    end
+  end
+
 end
